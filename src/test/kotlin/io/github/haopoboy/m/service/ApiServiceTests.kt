1fef0cd482e8720f6c90f9864379744b7bbc7661
haopo <haopoboy@gmail.com>
2020-04-02 20:44:02 +0800

    Quick assertions of checking keys

--- src/test/kotlin/io/github/haopoboy/m/service/ApiServiceTests.kt
+++ src/test/kotlin/io/github/haopoboy/m/service/ApiServiceTests.kt
@@ -17,34 +17,52 @@ class ApiServiceTests {
 
     @Autowired
     private lateinit var service: ApiService
+
     @Autowired
     private lateinit var initializer: DataInitializer
 
-    private var impl: ApiService.Named? = null
-    private var implMultiple: ApiService.Named? = null
+    private lateinit var impl: ApiService.Named
+    private lateinit var multipleImpl: ApiService.Named
+    private lateinit var nativeImpl: ApiService.Named
 
     @Before
     fun init() {
         initializer.import()
         impl = service.forName("resource")
-        implMultiple = service.forName("resourceMultiple")
+        multipleImpl = service.forName("resourceMultiple")
+        nativeImpl = service.forName("resourceNative")
     }
 
     @Test
-    fun queries() {
-        val page = impl?.query()!!.values.first()
-        assertThat(page).extracting("name").contains("resource", "resourceMultiple")
+    fun query() {
+        impl.query().values.first().apply {
+            assertThat(this).extracting("name").contains("resource", "resourceMultiple")
+        }
     }
 
     @Test
-    fun queriesWithCriteria() {
-        val page = impl?.query(mapOf("nice" to "job"))
+    fun queryNative() {
+        nativeImpl.query().apply {
+            assertThat(this).containsKey("list")
+        }
     }
 
     @Test
-    fun queriesMultiple() {
-        val map = implMultiple?.query()!!
-        println(map)
+    fun queryWithCriteria() {
+        impl.query(mapOf("name" to "job")).apply {
+            assertThat(this.values.first().content).isEmpty()
+        }
+        impl.query(mapOf("name" to "resource")).apply {
+            println(this.values.first().content)
+            assertThat(this.values.first().content).hasSize(1)
+        }
+    }
+
+    @Test
+    fun queryMultiple() {
+        multipleImpl.query().apply {
+            assertThat(this).containsKeys("list", "nativeList")
+        }
     }
 
 }
