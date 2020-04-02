68c609ffbb427a8b4795e6445415f41e45a1e3bc
haopo <haopoboy@gmail.com>
2020-04-02 15:23:25 +0800

    Support generating count query with sub query

--- src/main/kotlin/io/github/haopoboy/m/util/Queries.kt
+++ src/main/kotlin/io/github/haopoboy/m/util/Queries.kt
@@ -5,18 +5,19 @@ import javax.persistence.Query
 class Queries {
 
     companion object {
-        private val REPLACE_REGEX = "(?i)(SELECT)[^&]*(\\).?FROM)".toRegex()
+        private val REPLACE_REGEX = "(?i)FROM".toRegex()
 
-        fun applyCriteria(query: Query, criteria: Map<String, Any>) {
+        fun applyCriteria(query: Query, criteria: Map<String, Any?>) {
             query.parameters.forEach {
                 query.setParameter(it.name, criteria[it.name])
             }
         }
 
         fun replaceAsCountQuery(query: String): String {
+            val statements = query.split(REPLACE_REGEX)
             return """
                 SELECT count(*) FROM
-                ${query.replace(REPLACE_REGEX, "")}
+                ${statements.subList(1, statements.size).joinToString("FROM")}
             """.trimIndent()
         }
     }
--- src/test/kotlin/io/github/haopoboy/m/util/QueriesUnitTest.kt
+++ src/test/kotlin/io/github/haopoboy/m/util/QueriesUnitTest.kt
@@ -10,14 +10,55 @@ class QueriesUnitTest {
         "SELECT new Map(p.name AS name) FROM Person p".apply {
             assertThat(Queries.replaceAsCountQuery(this))
                     .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
+                    .contains("Person p")
+        }
+        "SELECT new Map(p.name AS name, p.name AS another) FROM Person p".apply {
+            assertThat(Queries.replaceAsCountQuery(this))
+                    .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
+                    .contains("Person p")
+        }
+        "SELECT p.name FROM Person p".apply {
+            assertThat(Queries.replaceAsCountQuery(this))
+                    .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
+                    .contains("Person p")
+        }
+        "SELECT p.name AS name FROM Person p".apply {
+            assertThat(Queries.replaceAsCountQuery(this))
+                    .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
                     .contains("Person p")
         }
 
         // Native
+        "SELECT p.name FROM Person p".apply {
+            assertThat(Queries.replaceAsCountQuery(this))
+                    .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
+                    .contains("Person p")
+        }
+
         "SELECT p.name AS name FROM Person p".apply {
             assertThat(Queries.replaceAsCountQuery(this))
                     .contains("SELECT count(*) FROM")
                     .contains("Person p")
+        }
+
+    }
+
+    @Test
+    fun replaceAsCountQueryWithSubQuery() {
+        """
+            SELECT new Map(p.name AS name) FROM Person p
+            WHERE exists(SELECT pp FROM Person pp WHERE pp.uuid = p.uuid)
+        """.trimIndent().apply {
+            assertThat(Queries.replaceAsCountQuery(this))
+                    .contains("SELECT count(*) FROM")
+                    .doesNotContain("p.name")
+                    .contains("Person p")
+                    .contains("exists(SELECT pp FROM Person pp WHERE pp.uuid = p.uuid)")
         }
     }
 }
